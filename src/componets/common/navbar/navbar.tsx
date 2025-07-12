e56d742885e63cdccea3f475be298c6a46cd26fe
paulkabzz <pkabulu05@gmail.com>
2025-07-12 21:38:31 +0200

    updated navbar

--- src/componets/common/navbar/navbar.tsx
+++ src/componets/common/navbar/navbar.tsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router";
 import styles from './index.module.css';
+import { Hamburger } from "../buttons/hamburger";
 
-interface NavLink {
+export interface NavLink {
     label: string;
     href: string;
 }
@@ -13,22 +14,22 @@ interface INavBar {
 const NavBar: React.FC<INavBar> = ({name, links}) => {
   return (
     <header className={styles.header}>
-        <section>
+        <h1 className={styles.name}>
             { name }
-        </section>
-        <section>
-            <nav>
-                <ul>
-                    {
-                        links.map((link, index: number) => (
-                            <li key={index}>
-                                <Link to={link.href}>{link.label}</Link>
-                            </li>
-                        ))
-                    }
-                </ul>
-            </nav>
-        </section>
+        </h1>
+
+        <Hamburger />
+        <nav className={styles.cont}>
+            <ul className={styles.linksContainer}>
+                {
+                    links.map((link, index: number) => (
+                        <li key={index}>
+                            <Link to={link.href}>{link.label}</Link>
+                        </li>
+                    ))
+                }
+            </ul>
+        </nav>
     </header>
   )
 }
--- src/root-layout.tsx
+++ src/root-layout.tsx
@@ -1,16 +1,33 @@
 import { href, Outlet } from "react-router";
-import NavBar from "./componets/common/navbar/navbar";
+import NavBar, { type NavLink } from "./componets/common/navbar/navbar";
 import { useAuth } from "./context/auth-context";
 import Footer from "./componets/common/footer/footer";
 
 const RootLayout: React.FC = (): React.ReactElement => {
     const { user } = useAuth();
-    const links = [
-        {
-            label: "Projects",
-            href: "#projects"
-        }
-    ]
+    const links: NavLink[] = [
+    {
+        label: "About",
+        href: "#about"
+    },
+    {
+        label: "Projects",
+        href: "#projects"
+    },
+    {
+        label: "Education",
+        href: "#education"
+    },
+    {
+        label: "Experience",
+        href: "#experience"
+    },
+    {
+        label: "Contact",
+        href: "/contact"
+    }
+    ];
+
   return (
     <>
       <NavBar name={user?.name ?? "John Doe"} links={links} />
